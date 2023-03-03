49bbb104189f32c5c30ab20c334833906b1cea76
devYuMinKim <gimyumin40@gmail.com>
2023-03-03 17:43:41 +0900

    Refactor : modify repository structure

--- _20230303/src/Math.java
+++ _20230303/src/Math.java
@@ -15,12 +15,12 @@ public class Math {
         int sum;
         Math math = new Math();
         sum = math.add(2, 3);
-        System.out.println("2와 3의 합은 " + sum + "입니다.");
+        System.out.println("2와 3의 합은 " + sum + "입니다."); // 2와 3의 합은 5입니다.
         sum = math.add(7, 8);
-        System.out.println("7과 8의 합은 " + sum + "입니다.");
+        System.out.println("7과 8의 합은 " + sum + "입니다."); // 7과 8의 합은 15입니다.
 
         Math obj = new Math();
-        System.out.println(obj.square(10));
-        System.out.println(obj.square(3.14));
+        System.out.println(obj.square(10));             // 100
+        System.out.println(obj.square(3.14));           // 9.8596
     }
 }
